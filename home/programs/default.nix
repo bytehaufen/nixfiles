9014fe7347bfca080ab6cf7608d6dc69e3cbb920
rico-de <rico.ukro@gmx.de>
2024-04-29 16:48:54 +0200

    add bat config to programs

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -3,6 +3,14 @@
 
   programs.home-manager.enable = true;
 
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "gruvbox-dark";
+      tabs = "2";
+    };
+  };
+
   programs.git.enable = true;
   #TODO: fix!
 }
