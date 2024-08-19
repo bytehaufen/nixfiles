23af9c1f36e0262b8afaceb74a2cbd71fdd8a69e
rico-de <rico.ukro@gmx.de>
2024-08-19 16:51:09 +0200

    wip: add default applications

--- home/terminal/programs/xdg.nix
+++ home/terminal/programs/xdg.nix
@@ -2,22 +2,46 @@
   config,
   pkgs,
   ...
-}: let
-  defaultApplications = {
-    # TODO: Add default applications
-  };
-in {
+}: {
   xdg = {
     enable = true;
     cacheHome = config.home.homeDirectory + "/.local/cache";
 
     mime.enable = true;
 
-    # FIXME: Extract this from ~/.config/mimeapps.list
-    # mimeApps = {
-    #   enable = true;
-    #   defaultApplications = defaultApplications;
-    # };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/epub+zip" = ["org.pwmt.zathura.desktop"];
+        "application/pdf" = ["org.pwmt.zathura.desktop"];
+        "application/vnd.ms-word.document.macroenabled.12" = ["libreoffice-writer.desktop"];
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["libreoffice-writer.desktop"];
+        "application/xhtml+xml" = ["org.qutebrowser.qutebrowser.desktop"];
+        "application/xml" = ["org.qutebrowser.qutebrowser.desktop"];
+        "image/gif" = ["vimiv.desktop"];
+        "image/jpeg" = ["vimiv.desktop"];
+        "image/png" = ["vimiv.desktop"];
+        "image/svg+xml" = ["vimiv.desktop"];
+        "image/webp" = ["vimiv.desktop"];
+        "inode/directory" = ["xplr.desktop"];
+        "text/csv" = ["nvim.desktop"];
+        "text/html" = ["org.qutebrowser.qutebrowser.desktop"];
+        "text/plain" = ["nvim.desktop"];
+        "text/tab-separated-values" = ["visidata.desktop"];
+        "text/x-c" = ["nvim.desktop"];
+        "text/x-diff" = ["nvim.desktop"];
+        "text/x-shellscript" = ["nvim.desktop"];
+        "video/mp4" = ["mpv.desktop"];
+        "video/x-matroska" = ["mpv.desktop"];
+        "x-scheme-handler/about" = ["org.qutebrowser.qutebrowser.desktop"];
+        "x-scheme-handler/http" = ["org.qutebrowser.qutebrowser.desktop"];
+        "x-scheme-handler/https" = ["org.qutebrowser.qutebrowser.desktop"];
+        "x-scheme-handler/unknown" = ["org.qutebrowser.qutebrowser.desktop"];
+        "x-scheme-handler/mailto" = ["userapp-Thunderbird-IGOQ71.desktop"];
+        "message/rfc822" = ["userapp-Thunderbird-IGOQ71.desktop"];
+        "x-scheme-handler/mid" = ["userapp-Thunderbird-IGOQ71.desktop"];
+      };
+    };
 
     systemDirs.data = ["${config.home.homeDirectory}/.nix-profile/share/applications" "${config.home.homeDirectory}/.nix-profile/share/"];
 
