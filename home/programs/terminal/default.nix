bc48cbbd459221d1d2ab46e94d7782ba2e853cff
rico-de <rico.ukro@gmx.de>
2024-08-20 14:06:36 +0200

    comment out broken igrep

--- home/programs/terminal/default.nix
+++ home/programs/terminal/default.nix
@@ -105,7 +105,7 @@ in {
     bat # Cat clone
     fd # Find clone
     zoxide # Cd clone
-    igrep # Interactive grep
+    # igrep # Interactive grep
     dust # Disk usage
     zoxide # Intelligent cd
     hexyl # Hex viewer
