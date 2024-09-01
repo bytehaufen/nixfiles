917ea1a659e92293a0613bfbd4af93f7642a923d
rico-de <rico.ukro@gmx.de>
2024-09-01 06:14:32 +0200

    reorder hyprland settings

--- home/programs/wayland/hyprland/default.nix
+++ home/programs/wayland/hyprland/default.nix
@@ -71,18 +71,6 @@ in {
     xwayland.enable = true;
 
     settings = {
-      env = [
-        "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
-        "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
-        "MOZ_WEBRENDER,1"
-        # misc
-        "_JAVA_AWT_WM_NONREPARENTING,1"
-        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        "QT_QPA_PLATFORM,wayland"
-        "SDL_VIDEODRIVER,wayland"
-        "GDK_BACKEND,wayland"
-      ];
-
       exec-once = [
         "swaybg -m fill -i ${config.stylix.image}"
       ];
