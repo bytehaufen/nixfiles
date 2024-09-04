b0aec77a37653bf831956eea4388cab451f7f30c
rico-de <rico.ukro@gmx.de>
2024-09-04 08:27:21 +0200

    feat: update mpv config

--- home/programs/gui/media/mpv.nix
+++ home/programs/gui/media/mpv.nix
@@ -2,6 +2,15 @@
   programs.mpv = {
     enable = true;
     defaultProfiles = ["gpu-hq"];
-    scripts = [pkgs.mpvScripts.mpris];
+    scripts = [
+      pkgs.mpvScripts.mpris # Control mpv with standard media keys
+    ];
+    config = {
+      keep-open = true;
+    };
+    bindings = {
+      "Alt+RIGHT" = "no-osd cycle-values video-rotate  '90' '180' '270' '0'";
+      "Alt+LEFT" = "no-osd cycle-values video-rotate  '270' '180' '90' '0'";
+    };
   };
 }
