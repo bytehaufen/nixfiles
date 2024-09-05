d462035f144a069511c1acbb09261a078174fd6d
rico-de <rico.ukro@gmx.de>
2024-09-05 06:50:20 +0200

    feat: add shell integration for starship

--- home/programs/tui/shell/starship.nix
+++ home/programs/tui/shell/starship.nix
@@ -3,6 +3,11 @@
 
   programs.starship = {
     enable = true;
+
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableNushellIntegration = true;
+
     settings = {
       character = {
         success_symbol = "[›](bold green)";
