7a15821539ab5716ca1fce926e449161bf28629f
rico-de <rico.ukro@gmx.de>
2024-09-08 08:34:28 +0200

    feat(neovim): add key config for diffview.nvim

--- home/programs/tui/neovim/nvim/lua/plugins/util.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/util.lua
@@ -2,7 +2,24 @@ return {
   -- git diff view
   {
     "sindrets/diffview.nvim",
+    dependencies = {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts_extend = { "spec" },
+      opts = {
+        spec = {
+            mode = { "n", "v" },
+            { "<leader>gd", group = "Diffview" },
+        },
+      },
+    },
     cmd = "DiffviewOpen",
+    keys = {
+      { "<leader>gdd", mode = { "n" }, "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gdh", mode = { "n" }, "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
+      { "<leader>gdq", mode = { "n" }, "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+      { "<leader>gde", mode = { "n" }, "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview Toggle Files" },
+    },
   },
   {
     "gennaro-tedesco/nvim-jqx",
