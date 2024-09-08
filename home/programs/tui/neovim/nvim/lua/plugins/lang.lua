4e70a28a14fce02db1f2cd944b4f2e0817b7bbc8
rico-de <rico.ukro@gmx.de>
2024-09-08 08:32:17 +0200

    feat(neovim): integrate nvim-treesitter config in lang.lua
    The nvim-treesitter configuration has been moved from its own file
    to the lang.lua file. This change simplifies the configuration and
    ensures all languages are installed by default. The previous
    configuration file for nvim-treesitter has been deleted.

--- home/programs/tui/neovim/nvim/lua/plugins/lang.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/lang.lua
@@ -1,20 +1,29 @@
 return {
-   -- Additional LSP's to be installed by mason
-   {
-     "williamboman/mason-lspconfig.nvim",
-     opts = function(_, opts)
-       if type(opts.ensure_installed) == "table" then
-         vim.list_extend(opts.ensure_installed, {
-           "bashls",
-           "dockerls",
-           "gradle_ls",
-           "html",
-           "arduino_language_server",
-           "docker_compose_language_service",
-         })
-       end
-       opts.automatic_installation = true
-       return opts
-     end,
-   },
+  -- Additional LSP's to be installed by mason
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "bashls",
+          "dockerls",
+          "gradle_ls",
+          "html",
+          "arduino_language_server",
+          "docker_compose_language_service",
+        })
+      end
+      opts.automatic_installation = true
+      return opts
+    end,
+  },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      -- Ensure all languages are installed who care disc space
+      ensure_installed = "all",
+    },
+  },
 }
