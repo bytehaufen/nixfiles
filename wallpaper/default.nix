1cc6056fb2c6189e174b1b8d2affc8fc1421ca3e
rico-de <rico.ukro@gmx.de>
2024-09-09 20:44:43 +0200

    fix: fixer file ending of linked wallpapers

--- wallpaper/default.nix
+++ wallpaper/default.nix
@@ -8,10 +8,10 @@
       Location of the wallpaper to use throughout the system.
     '';
     type = lib.types.path;
-    example = lib.literalExample ".config/wallpaper/wallpaper.png";
-    default = config.xdg.configHome + "/wallpaper/black_arch";
+    example = lib.literalExample "/home/foobar/.config/wallpaper/wallpaper.png";
+    default = config.xdg.configHome + "/wallpaper/black_arch.png";
   };
 
-  config.xdg.configFile."wallpaper/black_arch".source = ./black_arch.png;
-  config.xdg.configFile."wallpaper/avatar".source = ./avatar.jpg;
+  config.xdg.configFile."wallpaper/black_arch.png".source = ./black_arch.png;
+  config.xdg.configFile."wallpaper/avatar.jpg".source = ./avatar.jpg;
 }
