174b5c301e3234eb4dbc2b8508580bab43af971b
rico-de <rico.ukro@gmx.de>
2024-09-12 16:25:53 +0200

    feat: add 2 new wallpapers, and change default on hyprland

--- wallpaper/default.nix
+++ wallpaper/default.nix
@@ -9,9 +9,13 @@
     '';
     type = lib.types.path;
     example = lib.literalExample "/home/foobar/.config/wallpaper/wallpaper.png";
-    default = config.xdg.configHome + "/wallpaper/black_arch.png";
+    default = config.xdg.configHome + "/wallpaper/hyprland.png";
   };
 
-  config.xdg.configFile."wallpaper/black_arch.png".source = ./black_arch.png;
-  config.xdg.configFile."wallpaper/avatar.jpg".source = ./avatar.jpg;
+  config.xdg.configFile = {
+    "wallpaper/black_arch.png".source = ./black_arch.png;
+    "wallpaper/avatar.jpg".source = ./avatar.jpg;
+    "wallpaper/end_4.png".source = ./end_4.png;
+    "wallpaper/hyprland.png".source = ./hyprland.png;
+  };
 }
