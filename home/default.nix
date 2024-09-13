a8bb3c9ed47d831e7ab7dcf639cdb66633682dac
rico-de <rico.ukro@gmx.de>
2024-09-13 12:43:16 +0200

    feat: specify language env vars

--- home/default.nix
+++ home/default.nix
@@ -10,7 +10,17 @@
     homeDirectory = "/home/rico";
     stateVersion = "24.11";
     extraOutputsToInstall = ["doc" "devdoc"];
-    language.base = "en_US.UTF-8";
+    language = {
+      base = "en_US.UTF-8";
+
+      address = "de_DE.UTF-8";
+      measurement = "de_DE.UTF-8";
+      monetary = "de_DE.UTF-8";
+      name = "de_DE.UTF-8";
+      numeric = "de_DE.UTF-8";
+      paper = "de_DE.UTF-8";
+      telephone = "de_DE.UTF-8";
+    };
   };
   news.display = "silent";
   manual.manpages.enable = true;
