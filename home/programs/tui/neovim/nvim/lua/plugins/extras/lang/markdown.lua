8c356c0fd6b1b5f0607ccbaba7fe55b0ea7ef70e
rico-de <rico.ukro@gmx.de>
2024-09-13 16:10:08 +0200

    feat(markdown-preview.nvim): disable auto open of markdown preview

--- home/programs/tui/neovim/nvim/lua/plugins/extras/lang/markdown.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/extras/lang/markdown.lua
@@ -99,7 +99,7 @@ return {
         vim.g.mkdp_auto_close = 0
         vim.g.mkdp_combine_preview = 1
         vim.g.mkdp_combine_preview_auto_refresh = 1
-        vim.g.mkdp_auto_start = 1
+        vim.g.mkdp_auto_start = 0
       end
     end,
   },
