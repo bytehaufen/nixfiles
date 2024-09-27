80ec5a37b20856e439c3968292ff1c487d941a1c
bytehaufen <rico.ukro@gmx.de>
2024-09-27 12:05:18 +0200

    feat: make tokyonight transparent

--- home/programs/tui/neovim/nvim/lua/plugins/colorscheme.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/colorscheme.lua
@@ -3,20 +3,24 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "storm", -- [`storm`|`moon`|`night`|`day`]
-      transparent = false,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        sidebars = "dark",
-        floats = "dark",
-      },
-      hide_inactive_statusline = false,
-      dim_inactive = true,
-      lualine_bold = false,
-    },
+    opts = function(_, opts)
+      vim.g.tokyonight_dark_float = false
+
+      opts = {
+        style = "storm", -- [`storm`|`moon`|`night`|`day`]
+        -- FIXME: make this dependent on neovide
+        transparent = vim.g.neovide ~= true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+        },
+        hide_inactive_statusline = false,
+        dim_inactive = true,
+        lualine_bold = false,
+      }
+      return opts
+    end,
   },
 }
