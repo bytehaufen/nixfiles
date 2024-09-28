779c70f343e2e772ce43856a7f95c9a29e093b5b
bytehaufen <rico.ukro@gmx.de>
2024-09-28 14:34:23 +0200

    refactor: move gitdiffview into an extra git.lua

--- home/programs/tui/neovim/nvim/lua/plugins/util.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/util.lua
@@ -1,28 +1,9 @@
 return {
-  -- git diff view
-  {
-    "sindrets/diffview.nvim",
-    dependencies = {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts_extend = { "spec" },
-      opts = {
-        spec = {
-            mode = { "n", "v" },
-            { "<leader>gd", group = "Diffview" },
-        },
-      },
-    },
-    cmd = "DiffviewOpen",
-    keys = {
-      { "<leader>gdd", mode = { "n" }, "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
-      { "<leader>gdh", mode = { "n" }, "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
-      { "<leader>gdq", mode = { "n" }, "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
-      { "<leader>gde", mode = { "n" }, "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview Toggle Files" },
-    },
-  },
+  -- Browse and preview json files
+  -- Commands: `JqxList` and `JqxQuery`
   {
     "gennaro-tedesco/nvim-jqx",
+    event = { "BufReadPost" },
     ft = { "json", "yaml" },
   },
 }
