4b0bb12584763c16c31365901f2babd19d9b01cc
bytehaufen <rico.ukro@gmx.de>
2024-09-28 15:13:56 +0200

    revert: use flash instead of leap

--- home/programs/tui/neovim/nvim/lua/config/lazy.lua
+++ home/programs/tui/neovim/nvim/lua/config/lazy.lua
@@ -21,9 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
-    { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
-    -- TODO: Check harpoon
     { import = "lazyvim.plugins.extras.editor.overseer" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
