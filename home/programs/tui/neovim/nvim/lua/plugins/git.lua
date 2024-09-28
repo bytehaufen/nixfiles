fbaf17967a67137549c88ed75b501587818251bc
bytehaufen <rico.ukro@gmx.de>
2024-09-28 14:57:45 +0200

    feat: add git-conflict.nvim `<leader>gc`

--- home/programs/tui/neovim/nvim/lua/plugins/git.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/git.lua
@@ -2,23 +2,45 @@ return {
   -- Git diff view
   {
     "sindrets/diffview.nvim",
-    dependencies = {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts_extend = { "spec" },
-      opts = {
-        spec = {
-          mode = { "n", "v" },
-          { "<leader>gd", group = "Diffview" },
-        },
-      },
-    },
     cmd = "DiffviewOpen",
     keys = {
       { "<leader>gdd", mode = { "n" }, "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
       { "<leader>gdh", mode = { "n" }, "<cmd>DiffviewFileHistory<cr>", desc = "Diffview File History" },
       { "<leader>gdq", mode = { "n" }, "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
       { "<leader>gde", mode = { "n" }, "<cmd>DiffviewToggleFiles<cr>", desc = "Diffview Toggle Files" },
+    },
+  },
+
+  -- Git conflict
+  {
+    "akinsho/git-conflict.nvim",
+    lazy = false,
+    opts = {
+      default_mappings = {
+        ours = "<leader>gco",
+        theirs = "<leader>gct",
+        none = "<leader>gc0",
+        both = "<leader>gcb",
+        next = "]x",
+        prev = "[x",
+      },
+    },
+    keys = {
+      { "<leader>gcx", "<cmd>GitConflictListQf<cr>", desc = "List Conflicts" },
+      { "<leader>gcr", "<cmd>GitConflictRefresh<cr>", desc = "Refresh Conflicts" },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts_extend = { "spec" },
+    opts = {
+      spec = {
+        mode = { "n", "v" },
+        { "<leader>gd", group = "Diffview" },
+        { "<leader>gc", group = "Git Conflict" },
+      },
     },
   },
 }
