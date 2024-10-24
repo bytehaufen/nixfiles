780b03b20983cc978ad91c0f2d1f1a891588eaf5
bytehaufen <rico.ukro@gmx.de>
2024-10-24 08:36:24 +0200

    chore(refactor): improve QoC

--- home/programs/gui/emulators/default.nix
+++ home/programs/gui/emulators/default.nix
@@ -3,13 +3,13 @@
   pkgs-stable,
   ...
 }: {
-  home.packages = [
+  home.packages = with pkgs; [
     # NOTE: Stable because currently broken
     pkgs-stable.quickemu # Emulator manager
     pkgs-stable.quickgui # Emulator manager GUI
-    pkgs.spice-gtk # Spice client for quickemu
+    spice-gtk # Spice client for quickemu
 
-    pkgs.wineWowPackages.waylandFull # Windows compatibility layer
-    pkgs.winetricks # Windows compatibility layer
+    wineWowPackages.waylandFull # Windows compatibility layer
+    winetricks # Windows compatibility layer
   ];
 }
