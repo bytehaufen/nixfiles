19d8ff0f6d377272f931a56ea80e78aa8c63ce0a
bytehaufen <rico.ukro@gmx.de>
2024-11-02 17:11:28 +0100

    feat: add more mime types

--- home/programs/tui/xdg.nix
+++ home/programs/tui/xdg.nix
@@ -13,13 +13,14 @@
       enable = true;
       defaultApplications = {
         "application/epub+zip" = ["org.pwmt.zathura.desktop"];
+        "application/msword" = ["onlyoffice-desktopeditors.desktop"];
         "application/pdf" = ["onlyoffice-desktopeditors.desktop"];
-        "application/vnd.ms-word.document.macroenabled.12" = ["onlyoffice-desktopeditors.desktop"];
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["onlyoffice-desktopeditors.desktop"];
         "application/vnd.ms-excel" = ["onlyoffice-desktopeditors.desktop"];
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = ["onlyoffice-desktopeditors.desktop"];
         "application/vnd.ms-powerpoint" = ["onlyoffice-desktopeditors.desktop"];
+        "application/vnd.ms-word.document.macroenabled.12" = ["onlyoffice-desktopeditors.desktop"];
         "application/vnd.openxmlformats-officedocument.presentationml.presentation" = ["onlyoffice-desktopeditors.desktop"];
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = ["onlyoffice-desktopeditors.desktop"];
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ["onlyoffice-desktopeditors.desktop"];
         "application/xhtml+xml" = ["org.qutebrowser.qutebrowser.desktop"];
         "application/xml" = ["org.qutebrowser.qutebrowser.desktop"];
         "image/gif" = ["vimiv.desktop"];
@@ -28,6 +29,7 @@
         "image/svg+xml" = ["vimiv.desktop"];
         "image/webp" = ["vimiv.desktop"];
         "inode/directory" = ["xplr.desktop"];
+        "message/rfc822" = ["userapp-Thunderbird-IGOQ71.desktop"];
         "text/csv" = ["nvim.desktop"];
         "text/html" = ["org.qutebrowser.qutebrowser.desktop"];
         "text/plain" = ["nvim.desktop"];
@@ -40,10 +42,9 @@
         "x-scheme-handler/about" = ["org.qutebrowser.qutebrowser.desktop"];
         "x-scheme-handler/http" = ["org.qutebrowser.qutebrowser.desktop"];
         "x-scheme-handler/https" = ["org.qutebrowser.qutebrowser.desktop"];
-        "x-scheme-handler/unknown" = ["brave-browser.desktop"];
         "x-scheme-handler/mailto" = ["userapp-Thunderbird-IGOQ71.desktop"];
-        "message/rfc822" = ["userapp-Thunderbird-IGOQ71.desktop"];
         "x-scheme-handler/mid" = ["userapp-Thunderbird-IGOQ71.desktop"];
+        "x-scheme-handler/unknown" = ["brave-browser.desktop"];
       };
     };
 
