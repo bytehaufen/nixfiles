755f836cd549e1a41e01796a6c70d81df10b599f
bytehaufen <rico.ukro@gmx.de>
2024-11-16 08:32:23 +0100

    wip: fix not included nixosModules

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -3,23 +3,21 @@
   outputs,
   ...
 }: {
-  imports =
-    [
-      inputs.home-manager.nixosModules.home-manager
-      ./gamemode.nix
-      ./kdeconnect.nix
-      ./locale.nix
-      ./nix.nix
-      ./nix-ld.nix
-      ./openssh.nix
-      ./podman.nix
-      ./prometheus-node-exporter.nix
-      ./steam-hardware.nix
-      ./systemd-initrd.nix
-      ./upower.nix
-      ./zsh.nix
-    ]
-    ++ (builtins.attrValues outputs.nixosModules);
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ./gamemode.nix
+    ./kdeconnect.nix
+    ./locale.nix
+    ./nix.nix
+    ./nix-ld.nix
+    ./openssh.nix
+    ./podman.nix
+    ./prometheus-node-exporter.nix
+    ./steam-hardware.nix
+    ./systemd-initrd.nix
+    ./upower.nix
+    ./zsh.nix
+  ];
 
   home-manager = {
     useGlobalPkgs = true;
