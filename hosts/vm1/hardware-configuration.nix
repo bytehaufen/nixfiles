206d6f20e1de3928e68b0f57ec1c278a2eacba44
bytehaufen <rico.ukro@gmx.de>
2024-11-16 17:28:31 +0100

    fix vm1 hardware configuration

--- hosts/vm1/hardware-configuration.nix
+++ hosts/vm1/hardware-configuration.nix
@@ -1,32 +1,78 @@
 {
-  lib,
+  config,
   modulesPath,
   ...
-}: {
+}: let
+  hostname = config.networking.hostName;
+in {
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
   boot = {
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "ohci_pci"
-      "ehci_pci"
-      "virtio_pci"
-      "ahci"
-      "usbhid"
-      "sr_mod"
-      "virtio_blk"
-    ];
-    kernelModules = ["kvm-intel"];
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ohci_pci"
+        "ehci_pci"
+        "usb_storage"
+        "virtio_pci"
+        "ahci"
+        "usbhid"
+        "sr_mod"
+        "sd_mod"
+        "virtio_blk"
+      ];
+      kernelModules = ["kvm-intel"];
+    };
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max";
+      };
+      efi.canTouchEfiVariables = true;
+    };
   };
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/b2420af7-9f22-4ba0-b5c5-527332dacf17";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "ext4";
+      options = [
+        "subvol=root"
+      ];
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "ext4";
+      options = [
+        "subvol=nix"
+        "noatime"
+      ];
+    };
+
+    "/swap" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "ext4";
+      options = [
+        "subvol=swap"
+        "noatime"
+      ];
+    };
   };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9019-D296";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
   };
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 8196;
+    }
+  ];
+
+  nixpkgs.hostPlatform.system = "x86_64-linux";
 }
