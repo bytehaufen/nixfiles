fa70569c3ff42037d70f48577cef22f053ceb196
bytehaufen <rico.ukro@gmx.de>
2024-11-17 13:38:55 +0100

    add nix-community cachix

--- flake.nix
+++ flake.nix
@@ -29,9 +29,11 @@
   nixConfig = {
     extra-substituters = [
       "https://anyrun.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
