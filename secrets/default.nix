40cdf2fb064073cbf5c4c07ad46b4ac85e2009e6
bytehaufen <rico.ukro@gmx.de>
2024-11-28 14:13:34 +0100

    feat: add ssh key

--- secrets/default.nix
+++ secrets/default.nix
@@ -5,10 +5,6 @@
   config,
   ...
 }: let
-  noaccess = {
-    mode = "0000";
-    # owner = "root";
-  };
   high_security = {
     mode = "0500";
     owner = "root";
@@ -27,8 +23,34 @@ in {
   ];
 
   age = {
-    identityPaths = ["/etc/ssh/ssh_host_ed25519_key" "${config.home.homeDirectory}/.ssh/id_ed25519_rico"];
+    identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
+      "id_ed25519_rico" =
+        {
+          file = "${inputs.secrets}/id_ed25519_rico.age";
+        }
+        // high_security;
+
+      "id_ed25519_rico.pub" =
+        {
+          file = "${inputs.secrets}/id_ed25519_rico.pub.age";
+        }
+        // user_readable;
+    };
+  };
+
+  # Place secrets i /etc/
+  environment.etc = {
+    "ssh/id_ed25519_rico" = {
+      source = config.age.secrets."id_ed25519_rico".path;
+      mode = "0400";
+      user = "root";
+    };
+
+    "ssh/id_ed25519_rico.pub" = {
+      source = config.age.secrets."id_ed25519_rico.pub".path;
+      mode = "0400";
+      user = "root";
     };
   };
 }
