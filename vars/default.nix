2c298b4c37206bb0807ec2ceb20167be49fc3f4b
bytehaufen <rico.ukro@gmx.de>
2024-11-30 05:06:25 +0100

    feat: add public git key

--- vars/default.nix
+++ vars/default.nix
@@ -5,5 +5,6 @@
 
   sshAuthorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsiTAeXQT+Z48pFBddGwwRHbAHgUQLmvnn0S/0uO++b root@arch"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINW8KpRvhZm/cW4a5sys24HhfxDK+Ya/fJmU9vNIpA+b rico.ukro@gmx.de"
   ];
 }
