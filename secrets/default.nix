31206278f2439a3e57fbf7f185d9ef3a7e099318
bytehaufen <rico.ukro@gmx.de>
2024-12-07 08:18:35 +0100

    feat: reduce rights for secret files

--- secrets/default.nix
+++ secrets/default.nix
@@ -18,19 +18,34 @@
     secrets = {
       "id_ed25519_rico" = {
         file = "${inputs.secrets}/id_ed25519_rico.age";
-        mode = "0500";
+        mode = "0400";
         owner = vars.username;
       };
 
       "id_ed25519_rico.pub" = {
         file = "${inputs.secrets}/id_ed25519_rico.pub.age";
-        mode = "0500";
+        mode = "0400";
         owner = vars.username;
       };
+
+      "id_ed25519_rico_work" = {
+        file = "${inputs.secrets}/id_ed25519_rico_work.age";
+        mode = "0400";
+        owner = vars.username;
+      };
+
       "smb-secrets" = {
         file = "${inputs.secrets}/smb-secrets.age";
         mode = "0400";
         owner = "root";
+      };
+
+      "hosts-work" = {
+        file = "${inputs.secrets}/hosts-work.age";
+        path = "/etc/agenix/hosts-work";
+        mode = "0777";
+        owner = "rico";
+        symlink = false;
       };
     };
   };
@@ -45,6 +60,12 @@
 
     "ssh/id_ed25519_rico.pub" = {
       source = config.age.secrets."id_ed25519_rico.pub".path;
+      mode = "0400";
+      user = "${vars.username}";
+    };
+
+    "ssh/id_ed25519_rico_work" = {
+      source = config.age.secrets."id_ed25519_rico_work".path;
       mode = "0400";
       user = "${vars.username}";
     };
