aa91abafcb774edb131f6343d684d6aba673ae1d
bytehaufen <rico.ukro@gmx.de>
2024-12-07 08:19:30 +0100

    feat: add new system to authorized ssh keys

--- vars/default.nix
+++ vars/default.nix
@@ -6,5 +6,6 @@
   sshAuthorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsiTAeXQT+Z48pFBddGwwRHbAHgUQLmvnn0S/0uO++b root@arch"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINW8KpRvhZm/cW4a5sys24HhfxDK+Ya/fJmU9vNIpA+b rico.ukro@gmx.de"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXHvrsYdtmDFcGdpLiIz3qxQsr1iuFgWr2Y46u3YTyY rico@loki"
   ];
 }
