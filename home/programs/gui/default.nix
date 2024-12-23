86dae1ddd46a29556e9a7e2df026be8e936d40db
bytehaufen <rico.ukro@gmx.de>
2024-12-23 07:11:30 +0100

    feat: add obs plugins

--- home/programs/gui/default.nix
+++ home/programs/gui/default.nix
@@ -30,6 +30,28 @@
 
   programs.obs-studio = {
     enable = true;
-    plugins = [pkgs.obs-studio-plugins.wlrobs];
+    plugins = with pkgs.obs-studio-plugins; [
+      # Screen capture
+      wlrobs
+      obs-vaapi
+      obs-nvfbc
+      obs-teleport
+      droidcam-obs
+      obs-vkcapture
+      obs-gstreamer
+      obs-3d-effect
+      input-overlay
+      obs-multi-rtmp
+      obs-source-clone
+      obs-shaderfilter
+      obs-source-record
+      obs-livesplit-one
+      looking-glass-obs
+      obs-vintage-filter
+      obs-command-source
+      obs-move-transition
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
   };
 }
