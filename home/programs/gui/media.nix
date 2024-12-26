c4a35ba309f1d383ee1abd9a43a2f4d94d77c0d5
bytehaufen <rico.ukro@gmx.de>
2024-12-26 05:19:58 +0100

    feat: add alias for `pulsemixer` -> `pulse`

--- home/programs/gui/media.nix
+++ home/programs/gui/media.nix
@@ -10,18 +10,22 @@
     (config.lib.nixGL.wrap musicpod) # YT Music client
   ];
 
-  programs.mpv = {
-    enable = true;
-    defaultProfiles = ["gpu-hq"];
-    scripts = [
-      pkgs.mpvScripts.mpris # Control mpv with standard media keys
-    ];
-    config = {
-      keep-open = true;
-    };
-    bindings = {
-      "Alt+RIGHT" = "no-osd cycle-values video-rotate  '90' '180' '270' '0'";
-      "Alt+LEFT" = "no-osd cycle-values video-rotate  '270' '180' '90' '0'";
+  programs = {
+    zsh.shellAliases = {pulse = "pulsemixer";};
+
+    mpv = {
+      enable = true;
+      defaultProfiles = ["gpu-hq"];
+      scripts = [
+        pkgs.mpvScripts.mpris # Control mpv with standard media keys
+      ];
+      config = {
+        keep-open = true;
+      };
+      bindings = {
+        "Alt+RIGHT" = "no-osd cycle-values video-rotate  '90' '180' '270' '0'";
+        "Alt+LEFT" = "no-osd cycle-values video-rotate  '270' '180' '90' '0'";
+      };
     };
   };
 }
