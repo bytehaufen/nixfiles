3d6ea2a366d8703e8563de46a1003518a289a2f1
bytehaufen <rico.ukro@gmx.de>
2024-12-29 06:46:22 +0100

    feat: set hyprland monitors to auto values

--- home/rico/loki.nix
+++ home/rico/loki.nix
@@ -31,8 +31,8 @@
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1,1920x1080@60,0x0,1"
-      "HDMI-A-1,2560x1080@60,1920x0,1"
+      "eDP-1, preferred, auto, 1.2"
+      "HDMI-A-1, preferred, auto-right, 1"
     ];
   };
 
