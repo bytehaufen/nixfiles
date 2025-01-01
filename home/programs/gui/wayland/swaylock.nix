46463e59eb07fff192b591da2d8de9311c9b3dac
bytehaufen <rico.ukro@gmx.de>
2025-01-01 06:51:57 +0100

    feat(swaylock): enhance color scheme integration

--- home/programs/gui/wayland/swaylock.nix
+++ home/programs/gui/wayland/swaylock.nix
@@ -1,21 +1,55 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.colorScheme) palette;
+
+  primary = palette.base0A;
+  secondary = palette.base0C;
+  onSecondary = palette.base03;
+  surface = palette.base00;
+  surfaceBright = palette.base06;
+  error = palette.base0F;
+  onError = palette.base01;
+  onTertiary = palette.base01;
+in {
   programs.swaylock = {
-    # FIXME: Need to take systems `swaylock`,
-    # because home managers has no access to pam
     enable = true;
+    package = pkgs.swaylock-effects;
     settings = {
-      show-failed-attempts = true;
+      clock = true;
       image = "${config.opts.theme.wallpaper}";
+      effect-blur = "20x3";
+      fade-in = 0.1;
+
+      font-size = 20;
+
+      line-uses-inside = true;
+      disable-caps-lock-text = true;
+      ignore-empty-password = true;
+      indicator-caps-lock = true;
       indicator-radius = 100;
-      indicator-thickness = 7;
-      effect-blur = "7x5";
-      effect-vignette = "0.5:0.5";
-      ring-color = "fe8019";
-      key-hl-color = "fb4934";
-      line-color = "00000000";
-      inside-color = "28282866";
-      separator-color = "00000000";
-      fade-in = 0.2;
+      indicator-idle-visible = true;
+
+      ring-color = surfaceBright;
+      inside-wrong-color = onError;
+      ring-wrong-color = error;
+      key-hl-color = primary;
+      bs-hl-color = onTertiary;
+      ring-ver-color = secondary;
+      inside-ver-color = onSecondary;
+      inside-color = surface;
+      text-color = surfaceBright;
+      text-clear-color = surfaceBright;
+      text-ver-color = onSecondary;
+      text-wrong-color = error;
+      text-caps-lock-color = surfaceBright;
+      inside-clear-color = surface;
+      ring-clear-color = primary;
+      inside-caps-lock-color = onTertiary;
+      ring-caps-lock-color = surface;
+      separator-color = surface;
     };
   };
 }
