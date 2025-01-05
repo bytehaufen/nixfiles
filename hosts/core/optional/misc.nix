dcb94c162565520a4b21507603c63b83f72cb3f8
bytehaufen <rico.ukro@gmx.de>
2025-01-05 06:52:15 +0100

    feat(printing): enable discovery and use of network printers

--- hosts/core/optional/misc.nix
+++ hosts/core/optional/misc.nix
@@ -1,6 +1,19 @@
 {pkgs, ...}: {
   services = {
-    printing.enable = true; # Enable CUPS to print documents.
+    printing = {
+      enable = true; # Enable CUPS to print documents.
+      drivers = with pkgs; [gutenprint cups-filters epson-escpr];
+      browsing = true;
+      browsedConf = ''
+        BrowseDNSSDSubTypes _cups,_print
+        BrowseLocalProtocols all
+        BrowseRemoteProtocols all
+        CreateIPPPrinterQueues All
+        BrowseProtocols all
+      '';
+      defaultShared = true;
+    };
+
     geoclue2.enable = true; # Enable geolocation services.
 
     udev.packages = with pkgs; [
