4e9633c1f8f7bc7d6ce4da7f5bfc7024d2f5fbf3
bytehaufen <rico.ukro@gmx.de>
2025-01-05 07:05:10 +0100

    refactor(hyprland/scripts): make scripts executable

--- home/programs/gui/wayland/scripts/default.nix
+++ home/programs/gui/wayland/scripts/default.nix
@@ -1,10 +1,28 @@
 {
-  config.home.file = {
-    ".local/bin/pause-system".source = ./pause-system;
-    ".local/bin/record-area".source = ./record-area;
-    ".local/bin/screenshot-area".source = ./screenshot-area;
-    ".local/bin/set-monitors".source = ./set-monitors;
-    ".local/bin/set-monitors-mirrored".source = ./set-monitors-mirrored;
-    ".local/bin/toggle-lang".source = ./toggle-lang;
+  home.file = {
+    ".local/bin/pause-system" = {
+      source = ./pause-system;
+      executable = true;
+    };
+    ".local/bin/record-area" = {
+      source = ./record-area;
+      executable = true;
+    };
+    ".local/bin/screenshot-area" = {
+      source = ./screenshot-area;
+      executable = true;
+    };
+    ".local/bin/set-monitors" = {
+      source = ./set-monitors;
+      executable = true;
+    };
+    ".local/bin/set-monitors-mirrored" = {
+      source = ./set-monitors-mirrored;
+      executable = true;
+    };
+    ".local/bin/toggle-lang" = {
+      source = ./toggle-lang;
+      executable = true;
+    };
   };
 }
