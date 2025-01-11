31e240eda6d87402c8766d0681ca98e021373ed6
bytehaufen <rico.ukro@gmx.de>
2025-01-11 10:26:24 +0100

    feat: add gammastep for auto adjusting color temperature

---
+++ home/programs/gui/gammastep.nix
@@ -0,0 +1,26 @@
+{
+  # Adjust the color temperature(& brightness) of your screen
+  services.gammastep = {
+    enable = true;
+    # add a gammastep icon in the system tray
+    # has problem with wayland, so disable it
+    tray = true;
+    temperature = {
+      day = 6000;
+      night = 5000;
+    };
+    settings = {
+      general = {
+        fade = "1";
+        brightness-day = "1.0";
+        brightness-night = "0.8";
+        location-provider = "manual";
+      };
+      manual = {
+        # Germany, Dresden
+        lat = "51.0504"; # Latitude
+        lon = "13.7373"; # Longitude
+      };
+    };
+  };
+}
