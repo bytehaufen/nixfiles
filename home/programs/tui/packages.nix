56b0012f076cb8f47a4662a65616fbfd4c5f48c2
bytehaufen <rico.ukro@gmx.de>
2025-01-12 15:39:50 +0100

    feat: add nixd language-server

--- home/programs/tui/packages.nix
+++ home/programs/tui/packages.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     # Nix
     nil
+    nixd
     statix # Lints and suggestions for the nix programming language
     deadnix # Find and remove unused code in .nix source files
     alejandra # Nix Code Formatter
