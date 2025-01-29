2f3602058dd4094f3cae2c0dc1a57c0e926dd6ca
bytehaufen <rico.ukro@gmx.de>
2025-01-29 14:57:48 +0100

    feat(neovim): enhance markdown support with image plugins

--- home/programs/tui/neovim/nvim/lua/plugins/molten.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/molten.lua
@@ -10,20 +10,28 @@ return {
       },
     },
   },
-  -- Molten configuration
+  -- Molten configuration - Make neovim a jupyter notebook
   {
     "benlubas/molten-nvim",
     build = ":UpdateRemotePlugins", -- NOTE: Sometimes on first install, this has to be run manually
+    ft = { "markdown" },
+    dependencies = { "image.nvim" },
     config = function()
       vim.g.molten_auto_image_popup = true
-      vim.g.molten_cover_empty_lines = true
-      vim.g.molten_output_virt_lines = true
-      vim.g.molten_virt_text_output = true
-      vim.g.molten_auto_open_output = false
-      vim.g.molten_virt_lines_off_by_1 = true
-      vim.g.molten_copy_output = true
-      vim.g.molten_save_path = (vim.fn.stdpath("data") .. "/molten")
       vim.g.molten_auto_open_html_in_browser = true
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_copy_output = true
+      vim.g.molten_cover_empty_lines = true
+      vim.g.molten_image_location = "float"
+      vim.g.molten_output_virt_lines = true
+      vim.g.molten_output_win_border = { "", "━", "", "" }
+      vim.g.molten_output_win_max_height = 12
+      vim.g.molten_save_path = (vim.fn.stdpath("data") .. "/molten")
+      vim.g.molten_tick_rate = 142
+      vim.g.molten_use_border_highlights = true
+      vim.g.molten_virt_lines_off_by_1 = true
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_wrap_output = true
     end,
     keys = {
       { "<localleader>mr", mode = { "v" }, "<cmd>MoltenEvaluateVisual<cr>", desc = "evaluate visual selection" },
@@ -82,4 +90,42 @@ return {
       },
     },
   },
+  -- Save images from neovim with `:SaveImage`
+  {
+    "benlubas/image-save.nvim",
+    cmd = "SaveImage",
+  },
+  -- Render images in neovim
+  {
+    "3rd/image.nvim",
+    dependencies = { "https://github.com/leafo/magick" },
+    ft = { "markdown", "norg" },
+    config = function()
+      local image = require("image")
+
+      ---@diagnostic disable-next-line: missing-fields
+      image.setup({
+        backend = "kitty",
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = false,
+            only_render_image_at_cursor = false,
+            filetypes = { "markdown" }, -- markdown extensions (ie. quarto) can go here
+          },
+        },
+        max_width = 100,
+        max_height = 8,
+        max_height_window_percentage = math.huge,
+        max_width_window_percentage = math.huge,
+        window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+        editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+        tmux_show_only_in_active_window = true, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "fidget", "" },
+      })
+    end,
+  },
+  -- Render mermaid/plantuml/d2/gnuplot code fields as images into neovim
+  { "3rd/diagram.nvim", dependencies = { "image.nvim" }, enabled = false, dev = true, opts = {} },
 }
