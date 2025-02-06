25cce4bf9bbc798904b6db17fcac13c290eef05e
bytehaufen <rico.ukro@gmx.de>
2025-02-06 09:21:06 +0100

    feat(nvim): add toggle functionality for Precognition

--- home/programs/tui/neovim/nvim/lua/plugins/extras/editor/precognition.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/extras/editor/precognition.lua
@@ -1,3 +1,38 @@
+local enabled = false
+
 return {
-  { "tris203/precognition.nvim" },
+  {
+    "tris203/precognition.nvim",
+    opts = function()
+      local precognition = require("precognition")
+      Snacks.toggle({
+        name = "Precognition",
+        get = function()
+          return enabled
+        end,
+        set = function(state)
+          precognition.toggle()
+          enabled = state
+        end,
+      }):map("<leader>uP")
+
+      return { startVisible = enabled }
+    end,
+
+    event = "BufEnter",
+    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" },
+    keys = {
+      {
+        "<leader>uP",
+        function()
+          if require("precognition").toggle() then
+            LazyVim.info("Precognition is on")
+          else
+            LazyVim.warn("Precognition is off")
+          end
+        end,
+        desc = "toggle Precognition",
+      },
+    },
+  },
 }
