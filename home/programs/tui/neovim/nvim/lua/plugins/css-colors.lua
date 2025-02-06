203e09aa3e198549b552ea85358f9e23c1804f9c
bytehaufen <rico.ukro@gmx.de>
2025-02-06 14:48:58 +0100

    feat(nvim): update color picker and colorizer plugins

--- home/programs/tui/neovim/nvim/lua/plugins/css-colors.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/css-colors.lua
@@ -1,42 +1,42 @@
 return {
-  { -- Color picker plugin
-    -- In color picker popup change output with `o`
-    "ziontee113/color-picker.nvim",
-    config = function()
-      vim.keymap.set("i", "<C-c>", "<cmd>PickColorInsert<cr>", { noremap = true, silent = true })
-      require("color-picker").setup({
-        ["icons"] = { "·", "⸩" },
-      })
-    end,
+  {
+    "uga-rosa/ccc.nvim",
+    cmd = "CccPick",
+    keys = {
+      { "<leader>cP", "<Cmd>CccPick<CR>", desc = "Pick color" },
+    },
+    opts = {
+      highlighter = {
+        auto_enable = false,
+      },
+      point_char = "|",
+      empty_point_bg = false,
+      point_color_on_light = "#333333",
+    },
   },
   {
-    "NvChad/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({
-        filetypes = { "*" },
-        user_default_options = {
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          names = true, -- "Name" codes like Blue or blue
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          AARRGGBB = true, -- 0xAARRGGBB hex codes
-          rgb_fn = true, -- CSS rgb() and rgba() functions
-          hsl_fn = true, -- CSS hsl() and hsla() functions
-          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          -- Available modes for `mode`: foreground, background,  virtualtext
-          mode = "background", -- Set the display mode.
-          -- Available methods are false / true / "normal" / "lsp" / "both"
-          -- True is same as normal
-          tailwind = true, -- Enable tailwind colors
-          -- parsers can contain values used in |user_default_options|
-          sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
-          virtualtext = "■",
-          -- update color values even if buffer is not focused
-          -- example use: cmp_menu, cmp_docs
-          always_update = false,
-        },
-      })
-    end,
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = true, -- "Name" codes like Blue or blue
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = "background", -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = true, -- Enable tailwind colors
+        -- parsers can contain values used in |user_default_options|
+        sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+        always_update = false,
+      },
+    },
   },
 }
