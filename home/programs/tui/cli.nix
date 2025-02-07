17b2c7d4a79cf8bd6f6e3c83bd522fb2ee3f6e40
bytehaufen <rico.ukro@gmx.de>
2025-02-07 10:21:33 +0100

    feat(readline): enable vi mode

--- home/programs/tui/cli.nix
+++ home/programs/tui/cli.nix
@@ -18,5 +18,20 @@
 
   programs = {
     eza.enable = true;
+
+    readline = {
+      enable = true;
+      # Example how to set keybindings in inserts and command mode. Note that I'm ctrl-l to move in tmux terminal
+      # so doesn't work there.
+      extraConfig = ''
+        set editing-mode vi
+        $if mode=vi
+          set keymap vi-command
+          Control-l: clear-screen
+          set keymap vi-insert
+          Control-l: clear-screen
+        $endif
+      '';
+    };
   };
 }
