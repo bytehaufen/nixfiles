e62171e114f95f61a3696cae608548d017e6a7fe
bytehaufen <rico.ukro@gmx.de>
2025-02-13 08:25:40 +0100

    feat(nvim): use default lazyvim nix lang setup

--- home/programs/tui/neovim/nvim/lua/config/lazy.lua
+++ home/programs/tui/neovim/nvim/lua/config/lazy.lua
@@ -46,6 +46,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
--- home/programs/tui/neovim/nvim/lua/plugins/extras/lang/nix-extend.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/extras/lang/nix-extend.lua
@@ -1,20 +1,4 @@
 return {
-  recommended = {
-    ft = "nix",
-    root = "flake.nix",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "nix" } },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        nil_ls = {},
-      },
-    },
-  },
   {
     "stevearc/conform.nvim",
     optional = true,
