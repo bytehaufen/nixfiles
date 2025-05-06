7c807076bf5d245ef79145c2395432f6a610d31d
bytehaufen <rico.ukro@gmx.de>
2025-05-06 06:27:38 +0200

    feat: auto trigger hibernate on low battery level

--- hosts/core/global/upower.nix
+++ hosts/core/global/upower.nix
@@ -1,9 +1,15 @@
-{
+let
+  hibernatePercentage = 7;
+in {
   services = {
     power-profiles-daemon = {
       enable = true;
     };
     # DBus service that provides power management support to applications
-    upower.enable = true;
+    upower = {
+      enable = true;
+      criticalPowerAction = "Hibernate";
+      percentageAction = hibernatePercentage;
+    };
   };
 }
