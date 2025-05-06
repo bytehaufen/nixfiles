956fbe5fe4725c9777d03f58f7aef11d13d3a898
bytehaufen <rico.ukro@gmx.de>
2025-05-06 06:17:56 +0200

    feat(nvim): use nvim-ufo for auto folding in nvim-origami

--- home/programs/tui/neovim/nvim/lua/plugins/folding.lua
+++ home/programs/tui/neovim/nvim/lua/plugins/folding.lua
@@ -1,14 +1,23 @@
 return {
   { -- QoL features for folding
     "chrisgrieser/nvim-origami",
+    dependencies = { "kevinhwang91/nvim-ufo" },
     event = "VeryLazy",
     opts = {
-      keepFoldsAcrossSessions = true,
-      pauseFoldsOnSearch = true,
       foldKeymaps = {
         setup = true,
         hOnlyOpensOnFirstColumn = true,
       },
+      autoFold = {
+        enabled = true,
+        kinds = { "imports" }, ---@type lsp.FoldingRangeKind[]
+      },
+    },
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
     },
   },
 }
