e85bce511197893905232209137ecd334939fb11
bytehaufen <rico.ukro@gmx.de>
2025-05-18 13:01:45 +0200

    revert: secret handling is not convinient

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,98 +1,32 @@
-{
-  inputs,
-  pkgs,
-  vars,
-  config,
-  lib,
-  ...
-}: let
-  isNixOS = vars.isNixOS.content or vars.isNixOS;
-
-  agenixModule =
-    if isNixOS
-    then inputs.agenix.nixosModules.default
-    else inputs.agenix.homeManagerModules.default;
-
-  sshDir =
-    if isNixOS
-    then "ssh"
-    else ".ssh";
-  secretFiles = {
-    "${sshDir}/id_ed25519_rico" = {
-      source = config.age.secrets."id_ed25519_rico".path;
-      mode = "0400";
-      user = vars.username;
-    };
-
-    "${sshDir}/id_ed25519_rico.pub" = {
-      source = config.age.secrets."id_ed25519_rico.pub".path;
-      mode = "0400";
-      user = vars.username;
-    };
-
-    "${sshDir}/id_ed25519_rico_work" = {
-      source = config.age.secrets."id_ed25519_rico_work".path;
-      mode = "0400";
-      user = vars.username;
-    };
-  };
-in {
-  imports = [agenixModule];
-
-  environment = lib.mkIf isNixOS {
-    systemPackages = [
-      inputs.agenix.packages."${pkgs.system}".default
-    ];
-
-    # Place secrets in /etc/
-    etc = secretFiles;
-  };
-
-  home-manager.users.${vars.username} = lib.mkIf (!isNixOS) {
-    home.packages = [
-      inputs.agenix.packages."${pkgs.system}".default
-    ];
-
-    # Place secrets in /etc/
-    file = secretFiles;
-  };
-
+{inputs, ...}: {
   age = {
-    identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-    secretsDir = lib.mkIf (!isNixOS) "${config.home.homeDirectory}/.agenix/agenix";
-    secretsMountPoint = lib.mkIf (!isNixOS) "${config.home.homeDirectory}/.agenix/agenix.d";
     secrets = {
-      "id_ed25519_rico" = {
-        file = "${inputs.secrets}/id_ed25519_rico.age";
-        mode = "0400";
-        owner = vars.username;
-      };
-
-      "id_ed25519_rico.pub" = {
-        file = "${inputs.secrets}/id_ed25519_rico.pub.age";
-        mode = "0400";
-        owner = vars.username;
-      };
-
-      "id_ed25519_rico_work" = {
-        file = "${inputs.secrets}/id_ed25519_rico_work.age";
-        mode = "0400";
-        owner = vars.username;
-      };
-
+      # "id_ed25519_rico" = {
+      #   file = "${inputs.secrets}/id_ed25519_rico.age";
+      #   mode = "0400";
+      # };
+      #
+      # "id_ed25519_rico.pub" = {
+      #   file = "${inputs.secrets}/id_ed25519_rico.pub.age";
+      #   mode = "0400";
+      # };
+      #
+      # "id_ed25519_rico_work" = {
+      #   file = "${inputs.secrets}/id_ed25519_rico_work.age";
+      #   mode = "0400";
+      # };
+      #
       "smb-secrets" = {
         file = "${inputs.secrets}/smb-secrets.age";
         mode = "0400";
-        owner = "root";
       };
-
-      "hosts-work" = {
-        file = "${inputs.secrets}/hosts-work.age";
-        path = "/etc/agenix/hosts-work";
-        mode = "0777";
-        owner = "rico";
-        symlink = false;
-      };
+      #
+      # "hosts-work" = {
+      #   file = "${inputs.secrets}/hosts-work.age";
+      #   # path = "/etc/agenix/hosts-work";
+      #   mode = "0777";
+      #   # symlink = false;
+      # };
     };
   };
 }
--- vars/default.nix
+++ vars/default.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{
   username = "rico";
   gitName = "bytehaufen";
   email = "rico.ukro@gmx.de";
@@ -8,6 +8,4 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINW8KpRvhZm/cW4a5sys24HhfxDK+Ya/fJmU9vNIpA+b rico.ukro@gmx.de"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXHvrsYdtmDFcGdpLiIz3qxQsr1iuFgWr2Y46u3YTyY rico@loki"
   ];
-
-  isNixOS = lib.mkDefault true;
 }
