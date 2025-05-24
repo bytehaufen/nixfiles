25310926644d1f7d88d4f0ec819a1aab3310ab5a
bytehaufen <rico.ukro@gmx.de>
2025-05-24 17:03:11 +0200

    refactor(zsh): deactivate old zsh initextra

--- home/programs/tui/shell.nix
+++ home/programs/tui/shell.nix
@@ -53,37 +53,6 @@
       ZVM_VI_INSERT_ESCAPE_BINDKEY = "jk";
     };
 
-    # TODO: Refactor, e.g. ssh-agent is at the wrong place
-    initExtra =
-      # sh
-      ''
-        # TODO: Check obsolete
-        # Search history based on what's typed in the prompt
-        autoload -U history-search-end
-        zle -N history-beginning-search-backward-end history-search-end
-        zle -N history-beginning-search-forward-end history-search-end
-        bindkey "^[OA" history-beginning-search-backward-end
-        bindkey "^[OB" history-beginning-search-forward-end
-
-        # Open commands in $EDITOR with C-e
-        autoload -z edit-command-line
-        zle -N edit-command-line
-        bindkey "^e" edit-command-line
-
-        # case insensitive tab completion
-        zstyle ':completion:*' completer _complete _ignored _approximate
-        zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-        zstyle ':completion:*' menu select
-        zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-        zstyle ':completion:*' verbose true
-
-        # Use cache for completions
-        zstyle ':completion:*' use-cache on
-        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-        _comp_options+=(globdots)
-      '';
-
     envExtra =
       # sh
       ''
