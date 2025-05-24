81f096933feae500080050be49b63f4679708ab6
bytehaufen <rico.ukro@gmx.de>
2025-05-24 15:08:12 +0200

    chore(ssh-work): isolate configuration

--- home/rico/work/default.nix
+++ home/rico/work/default.nix
@@ -1,60 +1,11 @@
 {config, ...}: let
   identityFile = config.age.secrets.id_ed25519_rico_work.path;
-  localhost = "127.0.0.1";
 in {
   programs.ssh = {
-    matchBlocks = {
-      svn = {
-        inherit identityFile;
-        hostname = "zapp2";
-        user = "costun";
-        identitiesOnly = true;
-      };
-      jira-tunnel = {
-        inherit identityFile;
-        hostname = "kif";
-        user = "costun";
-        identitiesOnly = true;
-        localForwards = let
-          port = 58080;
-        in [
-          {
-            bind = {inherit port;};
-            host.address = localhost;
-            host = {inherit port;};
-          }
-        ];
-      };
-      confluence-tunnel = {
-        inherit identityFile;
-        hostname = "kif";
-        user = "costun";
-        identitiesOnly = true;
-        localForwards = let
-          port = 58090;
-        in [
-          {
-            bind = {inherit port;};
-            host.address = localhost;
-            host = {inherit port;};
-          }
-        ];
-      };
-      gitlab-tunnel = {
-        inherit identityFile;
-        hostname = "kif";
-        user = "costun";
-        identitiesOnly = true;
-        localForwards = let
-          port = 59080;
-        in [
-          {
-            bind = {inherit port;};
-            host.address = localhost;
-            host = {inherit port;};
-          }
-        ];
-      };
-    };
+    includes = [config.age.secrets.ssh-config-work.path];
+    extraConfig = ''
+      Host confluence-tunnel jira-tunnel gitlab-tunnel podman-registry-tunnel bender3 leela2 zoidberg leela kif elzar amy3 amy4 zapp zapp2 zappext svn-extern jenkins morbo nas smithers flanders svn
+        IdentityFile ${identityFile}
+    '';
   };
 }
