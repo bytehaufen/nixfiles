16822e7879bf2ad4c982eec970270d1f58cef8ab
bytehaufen <rico.ukro@gmx.de>
2025-05-25 06:06:20 +0200

    feat(system): do not set lang to use default en

--- home/rico/core.nix
+++ home/rico/core.nix
@@ -23,24 +23,6 @@
     homeDirectory = "/home/${vars.username}";
     stateVersion = "24.11";
     extraOutputsToInstall = ["doc" "devdoc"];
-
-    # Lang
-    sessionVariables = {
-      LANG = "en_US.UTF-8";
-      LC_ALL = "en_US.UTF-8";
-      LC_CTYPE = "en_US.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_COLLATE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-      LC_MESSAGES = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-    };
   };
   news.display = "silent";
   manual.manpages.enable = true;
