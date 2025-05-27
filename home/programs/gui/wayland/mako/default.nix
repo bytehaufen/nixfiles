531e433b1a5ecbce955db48192dfff4923b6b9ba
bytehaufen <rico.ukro@gmx.de>
2025-05-27 15:47:04 +0200

    chore: update make config to new api

--- home/programs/gui/wayland/mako/default.nix
+++ home/programs/gui/wayland/mako/default.nix
@@ -5,19 +5,19 @@
     enable = true;
 
     settings = {
-      iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
+      icon-path = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
       font = "${config.gtk.font.name} 12";
       padding = "10,20";
       anchor = "top-right";
       width = 400;
       height = 150;
-      borderSize = 2;
-      backgroundColor = "#${colors.base00}dd";
-      borderColor = "#${colors.base0A}dd";
-      textColor = "#${colors.base05}dd";
-      borderRadius = 5;
+      border-size = 2;
+      background-color = "#${colors.base00}dd";
+      border-color = "#${colors.base0A}dd";
+      text-color = "#${colors.base05}dd";
+      border-radius = 5;
       layer = "overlay";
-      maxHistory = 50;
+      max-history = 50;
     };
   };
 }
