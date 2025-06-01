0d3c6438747201f8248586147393018b549d4aa3
bytehaufen <rico.ukro@gmx.de>
2025-06-01 13:17:41 +0200

    refactor: make security enabled by gui

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -1,9 +1,15 @@
 {
-  security = {
-    pam.services.greetd.enableGnomeKeyring = true;
-    polkit.enable = true;
-  };
-  services = {
-    gnome.gnome-keyring.enable = true;
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.opts.nixos.gui.enable {
+    security = {
+      pam.services.greetd.enableGnomeKeyring = true;
+      polkit.enable = true;
+    };
+    services = {
+      gnome.gnome-keyring.enable = true;
+    };
   };
 }
