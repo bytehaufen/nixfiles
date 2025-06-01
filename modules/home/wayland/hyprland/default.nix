a58ca8bb90fa594f0749fed82d9bcefdd8359005
bytehaufen <rico.ukro@gmx.de>
2025-06-01 09:15:15 +0200

    refactor: make hyprland optional

--- modules/home/wayland/hyprland/default.nix
+++ modules/home/wayland/hyprland/default.nix
@@ -14,49 +14,51 @@
     (import ./rules.nix args)
   ];
 in {
-  # Propagate all scripts to the user env to make them available in the shell
-  home.packages = [pkgs.swaybg] ++ builtins.attrValues scripts;
+  config = lib.mkIf config.opts.hyprland.enable {
+    # Propagate all scripts to the user env to make them available in the shell
+    home.packages = [pkgs.swaybg] ++ builtins.attrValues scripts;
 
-  programs.zsh.shellAliases = {
-    xkill = "${lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl"} kill";
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-wlr
-    ];
-
-    config = {
-      common = {
-        default = [
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Secret" = [
-          "gnome-keyring"
-        ];
-      };
-      hyprland.default = ["gtk" "wlr"];
+    programs.zsh.shellAliases = {
+      xkill = "${lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl"} kill";
     };
-  };
 
-  wayland.windowManager.hyprland = {
-    inherit settings;
-
-    package = config.lib.nixGL.wrap (pkgs.hyprland.override {wrapRuntimeDeps = false;});
-    enable = true;
-
-    systemd = {
+    xdg.portal = {
       enable = true;
-      variables = ["--all"];
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-wlr
+      ];
+
+      config = {
+        common = {
+          default = [
+            "gtk"
+          ];
+          "org.freedesktop.impl.portal.Secret" = [
+            "gnome-keyring"
+          ];
+        };
+        hyprland.default = ["gtk" "wlr"];
+      };
     };
 
-    xwayland.enable = true;
-  };
+    wayland.windowManager.hyprland = {
+      inherit settings;
 
-  home.file.".wayland-session" = {
-    source = "${lib.getExe pkgs.hyprland}";
-    executable = true;
+      package = config.lib.nixGL.wrap (pkgs.hyprland.override {wrapRuntimeDeps = false;});
+      enable = true;
+
+      systemd = {
+        enable = true;
+        variables = ["--all"];
+      };
+
+      xwayland.enable = true;
+    };
+
+    home.file.".wayland-session" = {
+      source = "${lib.getExe pkgs.hyprland}";
+      executable = true;
+    };
   };
 }
