d3de554dbc4307a96899eba73ace625f3506c6e0
bytehaufen <rico.ukro@gmx.de>
2025-06-08 15:31:08 +0200

    fix(locale): correct i18n configuration initialization

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -3,25 +3,21 @@
   config,
   ...
 }: {
-  config =
-    {
-      i18n = {
-        defaultLocale = lib.mkDefault "en_US.UTF-8";
-        extraLocaleSettings = {
-          LC_TIME = lib.mkDefault "de_DE.UTF-8";
-        };
-        supportedLocales = lib.mkDefault [
-          "en_US.UTF-8/UTF-8"
-          "de_DE.UTF-8/UTF-8"
-        ];
-      };
-
-      time.timeZone = lib.mkDefault "Europe/Berlin";
-    }
-    // lib.mkIf config.opts.nixos.location.enable {
-      location.provider = "geoclue2";
-      services = {
-        geoclue2.enable = true; # Enable geolocation services.
-      };
+  i18n = {
+    defaultLocale = lib.mkDefault "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = lib.mkDefault "de_DE.UTF-8";
     };
+    supportedLocales = lib.mkDefault [
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+    ];
+  };
+
+  time.timeZone = lib.mkDefault "Europe/Berlin";
+
+  location.provider = lib.mkIf config.opts.nixos.location.enable "geoclue2";
+
+  # Enable geolocation services.
+  services.geoclue2.enable = lib.mkIf config.opts.nixos.location.enable true;
 }
