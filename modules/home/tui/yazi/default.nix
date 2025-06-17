e5090027b1e19284af0da0792117f11b2a732437
bytehaufen <rico.ukro@gmx.de>
2025-06-17 17:02:15 +0200

    feat(tui): update yazi settings: sort, hidden files, linemode

--- modules/home/tui/yazi/default.nix
+++ modules/home/tui/yazi/default.nix
@@ -15,12 +15,12 @@
     settings = {
       manager = {
         layout = [1 4 3];
-        sort_by = "alphabetical";
+        sort_by = "natural";
         sort_sensitive = false;
         sort_reverse = false;
         sort_dir_first = true;
-        linemode = "none";
-        show_hidden = false;
+        linemode = "mtime";
+        show_hidden = true;
         show_symlink = true;
       };
 
