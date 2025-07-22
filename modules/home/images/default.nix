2fa0057b55ea0db529a92af2699328abdcc7e62f
bytehaufen <rico.ukro@gmx.de>
2025-07-22 12:48:52 +0200

    fix: make images and keyboard proper optional

--- modules/home/images/default.nix
+++ modules/home/images/default.nix
@@ -1,8 +1,14 @@
 {
+  lib,
+  config,
+  ...
+}: {
   # Make my images available under $XDG_CONFIG_HOME/images, so that they can be
   # set manually in applications
-  config.xdg.configFile = {
-    "images/avatar.jpg".source = ./avatar.jpg;
-    "images/hyprland.png".source = ./hyprland.png;
+  config = lib.mkIf config.opts.home.gui.enable {
+    xdg.configFile = {
+      "images/avatar.jpg".source = ./avatar.jpg;
+      "images/hyprland.png".source = ./hyprland.png;
+    };
   };
 }
--- modules/home/keyboard/default.nix
+++ modules/home/keyboard/default.nix
@@ -1,10 +1,16 @@
 {
-  # My personal keyboard settings
-  # Contain an English and a German layout
-  # With <ESC> as <CAPS> and ` as <ESC> and ~ as <SHIFT>+<ESC>
-  # For my 65% keyboard
-  config.xdg.configFile = {
-    "xkb/symbols/de-65".source = ./de-65;
-    "xkb/symbols/en-65".source = ./en-65;
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.opts.home.windowManager.hyprland.enable {
+    # My personal keyboard settings
+    # Contain an English and a German layout
+    # With <ESC> as <CAPS> and ` as <ESC> and ~ as <SHIFT>+<ESC>
+    # For my 65% keyboard
+    xdg.configFile = {
+      "xkb/symbols/de-65".source = ./de-65;
+      "xkb/symbols/en-65".source = ./en-65;
+    };
   };
 }
